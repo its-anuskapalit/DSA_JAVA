8af1591922d4f61bbe3d6baad66eb7c657171c1a
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2025-06-26 16:51:54 +0530

    Create freqdistinct.java

---
+++ freqdistinct.java
@@ -0,0 +1,22 @@
+//===============Frequency of distinct elements==============================
+//import java.util.*;
+//class day{
+//    public static void main(String args[]){
+//        String str="abc##98550??ab";
+//        Set<Character> s=new HashSet<>();
+//        Set<Character> s1=new HashSet<>();
+//        Set<Character> s2=new HashSet<>();
+//        for(int i=0;i<str.length();i++){
+//            char c=str.charAt(i);
+//            if(Character.isDigit(c)){
+//                s.add(c);
+//            }else if(Character.isLetter(c)){
+//                s1.add(c);
+//            }
+//            else {
+//                s2.add(c);
+//            }
+//        }
+//        System.out.println(s.size()+s1.size()+ s2.size());
+//    }
+//}
